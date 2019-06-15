9a0f6030038467356923bd9e48db633c4ce1fbb2
Joel Hernandez <joel24hernan@gmail.com>
2019-06-15 11:34:31 -0500

    most updated version of the events from ticketmaster. Able to display the correct thumbnail and the name of the event. It also clears the div with the events when searching another city

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $("#submit-btn").on("click", function(event) {
     event.preventDefault();
-
+    $("#eventArea").empty();
 function displayApiData() {
     var location = $("#userInput").val().trim();
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events?size=5&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&city=(" + location + ")";
@@ -14,10 +14,27 @@ function displayApiData() {
         console.log(response)
         for (var i = 0; i < response.events.length; i++){
         console.log(response.events[i])
+
+        var eventDiv = $("<div>");
+        eventDiv.text(response.events[i].name)
+
+        for (var j = 0; j < response.events[i].images.length; j++) {
+            const element = response.events[i].images[j];
+            if (element.ratio === "4_3"){
+                var imageDiv = $("<img>");
+                imageDiv.attr("src",element.url);
+                eventDiv.append(imageDiv);
+                break;
+            }
+            console.log(element.url);  
         }
+        $("#eventArea").prepend(eventDiv);
+        
+        }  
+         
     });
 
     }
-
+    
     displayApiData();
 });
