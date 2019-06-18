6864acce78c9d6124f0d60f6528b347e03111462
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-18 11:37:57 -0500

    Added links to names of events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,7 +41,6 @@ $(document).on("click", "#submit-btn", function (event) {
     $("#error-input").empty();    
 
     //Added by Jyoti
-   debugger;
   var empty= document.forms["form1"]["text1"].value;
 
     if(empty==="")
@@ -145,9 +144,19 @@ function displayApiData() {
             eventDiv.addClass("event-div")
             var eventRow = $("<div>");
             eventRow.addClass("row");
+            var tickets = response.events[i].url;
             var name = response.events[i].name;
             var loc = response.events[i]._embedded.venues[0].address;
             var latLng = response.events[i]._embedded.venues[0].location;
+            console.log(name);
+
+            //  Turns event names into links to ticketmaster.
+            var linkName = $("<a>");
+            linkName.attr("href", tickets);
+            linkName.text("Name: " + name);
+            linkName.attr("target","_blank")
+            console.log(tickets);
+            
             console.log("address:" + JSON.stringify(loc));
             for (var j = 0; j < response.events[i].images.length; j++) {
                 const element = response.events[i].images[j];
@@ -179,20 +188,19 @@ function displayApiData() {
                         console.log("Time::::"+time1);
                         // p.html("Name: " + response.events[i].name + "<br>" + "Date: " + response.events[i].dates.start.localDate + "<br>" + "Time: " + response.events[i].dates.start.localTime + "<br>" + "Venue: " + response.events[i]._embedded.venues[0].name + "<br>")
                        //Added the date and time by jyoti
-                       p.html("Name: " + response.events[i].name + "<br>" + "Date: " + date1 + "<br>" + "Time: " + time1 + "<br>" + "Venue: " + response.events[i]._embedded.venues[0].name + "<br>")
+                    
+                       p.html("<br>" + "Date: " + date1 + "<br>" + "Time: " + time1 + "<br>" + "Venue: " + response.events[i]._embedded.venues[0].name + "<br>")
+                       p.prepend(linkName);
                         mapButton.text("Locate on map");
                         p.append(mapButton);
                         p.addClass("text col-6")
                         eventRow.append(p);
                         eventDiv.append(eventRow);
                         $("#eventArea").prepend(eventDiv);
-                        
-        
-
                     }
-
 });
     }
+
 //Added by Jyoti
 function geocodePlaceId(geocoder, map, infowindow) {
     console.log("LocalArray in set MAp()" + JSON.stringify(locArr));
